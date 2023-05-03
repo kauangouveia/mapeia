bc06ea68245b5b7eb91d2ff29144d60fb180bd1a
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-02 21:28:40 -0300

    chore: create component checkbox

---
+++ src/components/CheckBox/index.tsx
@@ -0,0 +1,18 @@
+import { useFormContext } from "react-hook-form";
+
+type checkBoxProps = {
+    id: string;
+    registerText: string;
+    value: string;
+    labelText: string;
+}
+
+export const Checkbox = ({ id, value, labelText, registerText }: checkBoxProps) => {
+    const { register } = useFormContext();
+    return (
+        <div className="w-52 h-10 pl-5 rounded flex justify-start items-center gap-3 font-semibold border-2 border-violet-700 mt-5">
+            <input type='checkbox' id={id} value={value}  {...register(registerText)} />
+            <label> {labelText} </ label>
+        </ div>
+    )
+}
