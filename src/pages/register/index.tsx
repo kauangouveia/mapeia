0604a0410b0e1dae81881c4375ae7db931adda53
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-02 21:28:21 -0300

    chore: create route register

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,5 +1,52 @@
+import { useNavigate } from "react-router-dom";
+import { Container } from "../../components/Container"
+import { MyInput as Input } from "../../components/Input"
+import { useForm, FormProvider } from "react-hook-form";
+import { Button } from "../../components/Button";
+import mapeiaLogo from "../../assets/mapeiaLogo.png"
+import { Checkbox } from "../../components/CheckBox";
+
+
+type RegisterProps = {
+  name: string;
+  email: string;
+  document: string;
+  password: string;
+  car: boolean;
+  motorcycle: boolean
+}
+
 export const Register = () => {
-    return (
-        <h1>Regiter</h1>
-    )
+  const methods = useForm<RegisterProps>();
+  const navigate = useNavigate()
+
+
+  const onSubmit = (data: RegisterProps) => {
+    navigate('/')
+    console.log(data)
+  }
+
+  return (
+    <Container>
+      <FormProvider {...methods}>
+        <form className="w-[700px] h-auto flex flex-col justify-center items-center gap-2"
+          onSubmit={methods.handleSubmit(onSubmit)}>
+          <img src={mapeiaLogo} alt="Logo" className="p-5" />
+          <div className="flex gap-4">
+            <Input size={200} textLabel="Name" textPlaceholder="User" registerText="name" type="text" />
+            <Input size={200} textLabel="Email" textPlaceholder="user@gmail.com" registerText="email" type="email" />
+          </div>
+          <div className="flex gap-4">
+            <Input size={200} textLabel="Password" textPlaceholder="***********" registerText="password" type="password" />
+            <Input size={200} textLabel="Document" textPlaceholder="xxx-xxx-xxx-xx" registerText="document" type="text" />
+          </div>
+          <div className="flex gap-4 mb-3">
+            <Checkbox id="CAR" labelText="CAR" value="true" registerText="CAR" />
+            <Checkbox id="MOTORCYCLE" labelText="MOTORCYCLE" value="true" registerText="MOTORCYCLE" />
+          </div>
+          <Button text="REGISTER" type="submit" />
+        </form>
+      </FormProvider>
+    </Container>
+  )
 }
