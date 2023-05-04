be87c61dbcfa039c050672273ea30841562a1451
kauan.gouveia <kauangouveia64@gmail.com>
2023-05-04 19:53:52 -0300

    chore: create test for buttons

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,9 +1,10 @@
 import { render } from '@testing-library/react'
-import Button from "./components/Button"
+import { Button } from "."
 
 test('Verify is type button and text in button', () => {
     const { getByText, debug } = render(<Button text='teste' type='submit' />)
+
     debug()
 
-    expect(getByText("teste")).toBeTruthy()
+    expect(getByText("teste")).toHaveAttribute('type', 'submit')
 })
