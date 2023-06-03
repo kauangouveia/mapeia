91ddde37acca9180707380c03c6c765804d3915a
kauan.gouveia <kauangouveia64@gmail.com>
2023-06-03 16:37:56 -0300

    fix: correct error off null register

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -8,10 +8,11 @@ type checkBoxProps = {
 }
 
 export const Checkbox = ({ id, value, labelText, registerText }: checkBoxProps) => {
-    const { register } = useFormContext();
+    const data = useFormContext();
+
     return (
         <div className="w-52 h-10 pl-5 rounded flex justify-start items-center gap-3 font-semibold border-2 border-violet-700 mt-5">
-            <input type='checkbox' id={id} value={value}  {...register(registerText)} />
+            <input type='checkbox' id={id} value={value}  {...data?.register(registerText)} />
             <label> {labelText} </ label>
         </ div>
     )
