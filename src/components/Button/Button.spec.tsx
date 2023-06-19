0699f150d624bb8ececf0c84f9aea97ddc8d1293
kauan.gouveia <kauangouveia64@gmail.com>
2023-06-19 16:21:41 -0300

    chore: remove example test

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react'
 import { Button } from "."
 
 test('Verify is type button and text in button', () => {
-    const { getByText, debug } = render(<Button text='text for test error' type='submit' />)
+    const { getByText, debug } = render(<Button text='text for test' type='submit' />)
 
     debug()
 
